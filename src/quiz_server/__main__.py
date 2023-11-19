468d7662f0db71661b0c85692968cb14524fb066
Martin Zeleny <mzeleny@redhat.com>
2023-11-19 21:43:08 +0100

    Day 3

--- src/quiz_server/__main__.py
+++ src/quiz_server/__main__.py
@@ -8,8 +8,6 @@ import signal
 from dataclasses import dataclass, field
 from ruamel.yaml import YAML
 
-from pprint import pprint
-
 
 @dataclass
 class Option:
@@ -31,9 +29,44 @@ class Question:
 class Quiz:
     name: str
     questions: list[Question] = field(default_factory=list)
+    current_question: int = 0
 
     def __post_init__(self):
         self.questions = [Question(**q) for q in self.questions]
+
+
+    def get_questions(self):
+        for order, question in enumerate(self.questions, start=1):
+            self.current_question = order
+            yield {'text': question.text,
+                   'options': [opt.answer for opt in question.options]}
+
+
+    def __len__(self):
+        return len(self.questions)
+
+
+@dataclass
+class Player:
+    id: str
+    websocket: WebSocket
+    score: int = 0
+
+
+class Players:
+    _players: list[Player] = []
+
+    def add(self, player: Player):
+        self._players.append(player)
+
+
+    def remove(self, player: Player):
+        self._players.remove(player)
+
+
+    async def send_question(self, question: dict):
+        for player in self._players:
+            await player.websocket.send_json(question)
 
 
 @asynccontextmanager
@@ -47,43 +80,52 @@ async def lifespan(app: FastAPI):
     with open(quiz_file, encoding='utf-8') as file:
         data = yaml.load(file)
 
-    quiz = Quiz(**data)
-    pprint(quiz)
-
+    app.state.quiz = Quiz(**data)
+    app.state.players = Players()
+    logger.warn("Registred players:")
     asyncio.ensure_future(control_server())
+
     yield
     logger.warn("\nBye!")
 
 
 app = FastAPI(lifespan=lifespan)
 logger = logging.getLogger(__name__)
-clients = {}
 
 
-@app.websocket("/ws/{client_id}")
-async def ws_endpoint(ws: WebSocket, client_id: str):
+@app.websocket("/register/{player_id}")
+async def register(ws: WebSocket, player_id: str):
     await ws.accept()
-    await ws.send_text(f"Welcome {client_id}!")
-    clients[client_id] = ws
-    print(f'\n{client_id}')
+    await ws.send_json(app.state.quiz.name)
+    await ws.send_json("Check your name on the screen!")
+
+    player = Player(player_id, ws)
+    app.state.players.add(player)
+
+    logger.warn(f'{player_id}')
 
     try:
         while True:
             data = await ws.receive_text()
-            print(f"\nClient {client_id} {ws}: {data}")
+            print(f"\nClient {player_id} {ws}: {data}")
     except WebSocketDisconnect:
-        del clients[client_id]
-
-
-async def send_to_clients(msg: str):
-    for client_socket in clients.values():
-        await client_socket.send_text(msg)
+        logger.warn(f"{player_id} disconected")
+        app.state.players.remove(player)
 
 
 async def control_server():
+    questions = app.state.quiz.get_questions()
+
     while True:
         proceed_char = await aioconsole.ainput("Proceed [y/N]: ")
         if proceed_char.lower() != 'y':
             continue
-        print("Otazka poslana")
-        await send_to_clients("Otazka")
+
+        try:
+            question = next(questions)
+            print(f'{app.state.quiz.current_question}/{len(app.state.quiz)}')
+        except StopIteration:
+            question = "Quiz ended"
+
+        logger.warn(question)
+        await app.state.players.send_question(question)
