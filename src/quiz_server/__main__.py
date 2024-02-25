1fa125369cd25811ab28998cdec469fad7c2e6bb
Martin Zeleny <mzeleny@redhat.com>
2024-02-25 21:31:16 +0100

    Refactoring

--- src/quiz_server/__main__.py
+++ src/quiz_server/__main__.py
@@ -23,8 +23,22 @@ class Question:
     time_limit: int | None = None
     options: list[Option] = field(default_factory=list)
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         self.options = [Option(**opt) for opt in self.options]
+
+    def print(self) -> None:
+        '''Nicely print text of the question with possible answeres'''
+
+        question_label = f'Question number {app.state.quiz.current_question}/{len(app.state.quiz)}'
+        logging.info(question_label)
+        print(f'\n{question_label}')
+
+        logging.info(f'Question text: {self.text}')
+        print(self.text)
+
+        for letter, opt in zip(string.ascii_letters, self.options):
+            logging.info(opt)
+            print(f'\t{letter}) {opt.answer}')
 
     def ask(self) -> dict:
         return {
@@ -62,13 +76,22 @@ class Quiz:
 
 @dataclass
 class Player:
-    id: str
-    websocket: WebSocket
+    _id: str
+    _websocket: WebSocket
     score: int = 0
+
+    async def send(self, data: dict):
+        await self._websocket.send_json(data)
+
+    async def close_connection(self, msg: str):
+        await self._websocket.close(reason=msg)
 
 
 class Players:
     _players: list[Player] = []
+
+    def find(self, player_id: str):
+        ...
 
     def add(self, player: Player):
         self._players.append(player)
@@ -76,21 +99,22 @@ class Players:
     def remove(self, player: Player):
         self._players.remove(player)
 
-    async def send(self, question: dict):
+    async def send(self, data: dict):
         for player in self._players:
-            await player.websocket.send_json(question)
+            await player.send(data)
 
-    async def close_connection(self, msg):
-        '''Print results of the quiz'''
+    async def close_connection(self, msg: str):
+        '''Print results of the quiz and disconnect the clients'''
+
         for player in self._players:
-            await player.websocket.close(reason=msg)
+            await player.close_connection(msg)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     quiz_file = os.environ.get('QUIZ')
     if not quiz_file:
-        shutdown("Environment variable QUIZ was not set!")
+        shutdown_server("Environment variable QUIZ was not set!")
 
     yaml = YAML(typ='safe')
     with open(quiz_file, encoding='utf-8') as file:
@@ -102,14 +126,14 @@ async def lifespan(app: FastAPI):
     asyncio.ensure_future(control_server())
 
     yield  # Second half of a life span - this is executed once server exits
-    shutdown("Quiz server quit")
+    shutdown_server("Quiz server quit")
 
 
 app = FastAPI(lifespan=lifespan)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(message)s",
-    filename=datetime.now().strftime("quiz-log_%Y-%m-%d_%H:%M:%S.txt")
+    filename=f'{datetime.now():quiz-log_%Y-%m-%d_%H:%M:%S.txt}'
 )
 
 
@@ -128,19 +152,18 @@ async def register(ws: WebSocket, player_id: str):
 
     try:
         while True:
-            data = await ws.receive_text()
+            data = await ws.receive_json()
             logging.info(f"Client {player_id} sent: {data}")
     except WebSocketDisconnect:
         logging.info(f"{player_id} has disconected")
         app.state.players.remove(player)
 
 
-async def control_server():
+async def control_server() -> None:
     '''
     Interaction with server app in terminal happens here. Joining players and
     sending question is possible in the same time
     '''
-
     print("Registred players:")
 
     while True:
@@ -151,31 +174,18 @@ async def control_server():
         try:
             question = next(app.state.quiz)
         except StopIteration:
+            app.state.quiz.print_results()
+
             msg = "Quiz ended"
             await app.state.players.close_connection(msg)
-            app.state.quiz.print_results()
-            shutdown(msg)
 
-        print_question(question)
+            shutdown_server(msg)
+
+        question.print()
         await app.state.players.send(question.ask())
 
 
-def print_question(question: Question):
-    '''Nicely print text of the question with possible answeres'''
-
-    question_label = f'Question number {app.state.quiz.current_question}/{len(app.state.quiz)}'
-    logging.info(question_label)
-    print(f'\n{question_label}')
-
-    logging.info(f'Question text: {question.text}')
-    print(question.text)
-
-    for letter, opt in zip(string.ascii_letters, question.options):
-        logging.info(opt)
-        print(f'\t{letter}) {opt.answer}')
-
-
-def shutdown(msg):
+def shutdown_server(msg: str) -> None:
     '''This way of exit might not be correct but fits the usage of this software'''
 
     logging.info(msg)
